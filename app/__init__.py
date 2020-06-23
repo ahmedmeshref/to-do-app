e3fd9146d3681f2125a3a35c9c94d749cc3f8eef
ahmedmeshref <a.meshref@alustudent.com>
2020-06-23 02:38:36 +0200

    Setup database file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+# set the db URI to the postgresql os variable + the db name
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('POSTGRESQL') + "todo"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = os.environ.get('SECRETKEY')
 
+db = SQLAlchemy(app)
 
-
+from app import views
