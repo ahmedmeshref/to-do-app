66767a9947db7ff2b61d8aa923cb8fec967906bf
ahmedmeshref <a.meshref@alustudent.com>
2020-06-27 02:58:18 +0200

    Implement a better selector on delete_task to choose the li item

--- app/static/delete_task.js
+++ app/static/delete_task.js
@@ -2,14 +2,13 @@ import {handleErrors} from "./utils.js";
 
 let app = {
     delete_btns: document.querySelectorAll(".delete-task"),
-    task_item: document.querySelectorAll(".task-item")
 }
 
 // delete task from db
 for (let i = 0; i < app.delete_btns.length; i++) {
     const btn = app.delete_btns[i];
     btn.onclick = () => {
-        let btn_id = btn.getAttribute("data-id");
+        const btn_id = btn.getAttribute("data-id");
         fetch(`/todos/${btn_id}/delete_task`, {
             method: 'POST',
             body: "delete-item",
@@ -20,7 +19,7 @@ for (let i = 0; i < app.delete_btns.length; i++) {
         .then(handleErrors)
         .then((responseVal) => {
             console.log(responseVal);
-            const task_item = app.task_item[i];
+            const task_item = document.querySelector(`li[data-id='${btn_id}']`);
             task_item.parentNode.removeChild(task_item);
         })
         .catch((err) => {
