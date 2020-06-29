b76fa673acbafbc7e1937e7f5c1ff06b78ac060b
ahmedmeshref <a.meshref@alustudent.com>
2020-06-29 06:38:27 +0200

    add Delete_list view implementation

--- app/static/delete_task.js
+++ app/static/delete_task.js
@@ -1,28 +1,42 @@
 import {handleErrors, logError, request} from "./utils.js";
 
 let app = {
-    delete_btns: document.querySelectorAll(".delete-task")
+    delete_task: document.querySelectorAll(".delete-task"),
+    delete_list: document.querySelectorAll(".delete-list")
 }
 
-let delete_item = (btn) => {
+let delete_item = (btn, address, wrapper) => {
     const btn_id = btn.getAttribute("data-id"),
-        route = `/todos/${btn_id}/delete_task`,
+        route = `/todos/${btn_id}/${address}`,
         method = "DELETE",
         handleRequestOutput = (responseVal) => {
             console.log(responseVal);
             // delete li item from dom
-            const all_tasks = document.querySelector("#tasks-wrapper"),
-                task_item = all_tasks.querySelector(`li[data-id='${btn_id}']`);
-            task_item.parentNode.removeChild(task_item);
+            const items_wrapper = document.querySelector(`#${wrapper}`),
+                target_item = items_wrapper.querySelector(`li[data-id='${btn_id}']`);
+            target_item.parentNode.removeChild(target_item);
         };
     request(route, method, {}, handleErrors, handleRequestOutput,  logError);
 }
 
 
-// delete task from db
-for (let i = 0; i < app.delete_btns.length; i++) {
-    const btn = app.delete_btns[i];
+// loop through all task keys looking for an click event
+for (let i = 0; i < app.delete_task.length; i++) {
+    const btn = app.delete_task[i];
     btn.onclick = () => {
-        delete_item(btn);
+        const address = 'delete_task',
+            wrapper = 'tasks-wrapper';
+        delete_item(btn, address, wrapper);
     }
+}
+
+// loop through all list keys looking for an click event
+for (let i= 0; i < app.delete_list.length; i++){
+    const btn = app.delete_list[i],
+        wrapper = 'lists-wrapper';
+    btn.addEventListener('click', () => {
+        const address = 'delete_list',
+            wrapper = 'lists-wrapper';
+        delete_item(btn, address, wrapper)
+    })
 }
