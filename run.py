e4d224fe57ebeed661a416e505a332a39792cffa
ahmedmeshref <a.meshref@alustudent.com>
2020-06-30 04:48:28 +0200

    Adding new list for a specific list fixed

--- run.py
+++ run.py
@@ -2,3 +2,9 @@ from app import app
 
 if __name__ == '__main__':
     app.run()
+
+"""
+Todo:
+1- When the user deletes the current selected list, the page refreshes 
+2- When you add a new task, you should be able to delete it and change its state 
+"""
